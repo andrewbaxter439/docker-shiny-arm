5f9ce601f0c79cb4d1a6ace12f653f1694efa4d6
Andy Baxter <andrewbaxter439@gmail.com>
2023-08-17 22:28:39 +0100

    missing files

---
+++ install_dependencies.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+SHINY_SERVER_VERSION=${1:-${SHINY_SERVER_VERSION:-latest}}
+
+## build ARGs
+NCPUS=${NCPUS:--1}
+
+# a function to install apt packages only if they are not installed
+function apt_install() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+            apt-get update
+        fi
+        apt-get install -y --no-install-recommends "$@"
+    fi
+}
+
+
+apt_install \
+    git \
+    xz-utils \
+    cmake \
+    g++ \
+    sudo \
+    gcc \
+    gdebi-core \
+    lsb-release \
+    libcurl4-openssl-dev \
+    libcairo2-dev \
+    libxt-dev \
+    xtail \
+    wget
+
+# Run dependency scripts
+# /rocker_scripts/install_s6init.sh
+/rocker_scripts/install_pandoc.sh
---
+++ install_node.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Find the relevant checksum from:
+# https://nodejs.org/dist/v{VERSION}/SHASUMS256.txt
+# The node-v{VERSION}-linux-x64.tar.xz checksum is the one we need.
+NODE_SHA256=aca4794ca60f2e17689a700eeaa95d7adcd5fd01cdd2e4f9d596ac41b600b796
+
+cd /shiny-server
+
+NODE_VERSION=$(cat .nvmrc)
+
+check_node_needed () {
+  if [ -x ext/node/bin/node ]
+  then
+    local CURRENT_NODE_VERSION=$(ext/node/bin/node --version 2>/dev/null)
+    if [[ "$CURRENT_NODE_VERSION" == "$NODE_VERSION" ]]
+    then
+      echo "Node $NODE_VERSION is already installed, skipping" >&2
+      exit 0
+    fi
+  fi
+}
+
+verify_checksum () {
+  local FILE=$1
+  local EXPECTED_CHECKSUM=$2
+
+  local ACTUAL_CHECKSUM=$(sha256sum "$FILE")
+  [[ "$EXPECTED_CHECKSUM  $FILE" != "$ACTUAL_CHECKSUM" ]]
+}
+
+download_node () {
+  local NODE_FILENAME="node-${NODE_VERSION}-linux-arm64.tar.xz"
+  local NODE_URL="https://nodejs.org/dist/${NODE_VERSION}/${NODE_FILENAME}"
+  local NODE_ARCHIVE_DEST="/tmp/${NODE_FILENAME}"
+  echo "Downloading Node ${NODE_VERSION} from ${NODE_URL}"
+
+  wget -O "$NODE_ARCHIVE_DEST" "$NODE_URL"
+  if verify_checksum "$NODE_ARCHIVE_DEST" "$NODE_SHA256"
+  then
+    echo "Checksum failed!" >&2
+    exit 1
+  fi
+
+  rm -rf ext/node
+  mkdir -p ext/node
+  echo "Extracting ${NODE_FILENAME}"
+  tar xf "${NODE_ARCHIVE_DEST}" --strip-components=1 -C "ext/node"
+
+  # Clean up temp file
+  rm "${NODE_ARCHIVE_DEST}"
+
+  cp ext/node/bin/node ext/node/bin/shiny-server
+  rm ext/node/bin/npm
+}
+
+check_node_needed
+download_node
