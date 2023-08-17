d10ba9f595796872d73147073e248ff32793a97a
Andy Baxter <andrewbaxter439@gmail.com>
2023-08-17 22:19:27 +0100

    files working on pi

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,14 @@ ENV S6_VERSION=v2.1.0.2
 ENV SHINY_SERVER_VERSION=latest
 ENV PANDOC_VERSION=default
 
-COPY install_shiny_server.sh /rocker_scripts/install_shiny_server.sh
+COPY install_shiny_server.sh install_dependencies.sh install_node.sh /rocker_scripts/
+RUN chmod 777 /rocker_scripts/install_node.sh
 RUN chmod 777 /rocker_scripts/install_shiny_server.sh
-# RUN /rocker_scripts/install_tidyverse.sh
+RUN chmod 777 /rocker_scripts/install_dependencies.sh
+
+RUN /rocker_scripts/install_dependencies.sh
 RUN /rocker_scripts/install_shiny_server.sh
 
 EXPOSE 3838
 
-CMD ["/init"]
+CMD ["/usr/bin/shiny-server"]
--- install_shiny_server.sh
+++ install_shiny_server.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 set -e
 
-SHINY_SERVER_VERSION=${1:-${SHINY_SERVER_VERSION:-latest}}
-
-## build ARGs
-NCPUS=${NCPUS:--1}
-
-# a function to install apt packages only if they are not installed
-function apt_install() {
-    if ! dpkg -s "$@" >/dev/null 2>&1; then
-        if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-            apt-get update
-        fi
-        apt-get install -y --no-install-recommends "$@"
-    fi
-}
-
-
-apt_install \
-    git \
-    xz-utils \
-    cmake \
-    g++ \
-    gcc \
-    sudo \
-    gdebi-core \
-    lsb-release \
-    libcurl4-openssl-dev \
-    libcairo2-dev \
-    libxt-dev \
-    xtail \
-    wget
-
-# Run dependency scripts
-/rocker_scripts/install_s6init.sh
-/rocker_scripts/install_pandoc.sh
-
 # Install Shiny server
 
 git clone https://github.com/rstudio/shiny-server.git
@@ -45,8 +10,9 @@ cd shiny-server
 mkdir tmp
 cd tmp
 
+
 # Install our private copy of Node.js
-../external/node/install-node.sh
+/rocker_scripts/install_node.sh
 
 # Add the bin directory to the path so we can reference node
 DIR=`pwd`
@@ -70,48 +36,16 @@ sudo make install
 sudo mkdir -p /etc/shiny-server
 sudo cp ../config/default.config /etc/shiny-server/shiny-server.conf
 
-# if [ "$SHINY_SERVER_VERSION" = "latest" ]; then
-#     SHINY_SERVER_VERSION=$(wget -qO- https://download3.rstudio.org/ubuntu-18.04/x86_64/VERSION)
-# fi
-# 
-# wget --no-verbose "https://download3.rstudio.org/ubuntu-18.04/x86_64/shiny-server-${SHINY_SERVER_VERSION}-amd64.deb" -O ss-latest.deb
-# gdebi -n ss-latest.deb
-# rm ss-latest.deb
-# 
-# # Get R packages
-# install2.r --error --skipinstalled -n "$NCPUS" shiny rmarkdown
-# 
-# Set up directories and permissions
-if [ -x "$(command -v rstudio-server)" ]; then
-    DEFAULT_USER=${DEFAULT_USER:-rstudio}
-    adduser "${DEFAULT_USER}" shiny
-fi
+# Place a shortcut to the shiny-server executable in /usr/bin
+sudo ln -s /usr/local/shiny-server/bin/shiny-server /usr/bin/shiny-server
 
-cp -R /usr/local/lib/R/site-library/shiny/examples/* /srv/shiny-server/
-chown shiny:shiny /var/lib/shiny-server
-mkdir -p /var/log/shiny-server
-chown shiny:shiny /var/log/shiny-server
+# Create shiny user. On some systems, you may need to specify the full path to 'useradd'
+sudo useradd -r -m shiny
 
-# create init scripts
-mkdir -p /etc/services.d/shiny-server
-cat <<"EOF" >/etc/services.d/shiny-server/run
-#!/usr/bin/with-contenv bash
-## load /etc/environment vars first:
-for line in $( cat /etc/environment ) ; do export $line > /dev/null; done
-if [ "$APPLICATION_LOGS_TO_STDOUT" != "false" ]; then
-    exec xtail /var/log/shiny-server/ &
-fi
-exec shiny-server 2>&1
-EOF
-chmod +x /etc/services.d/shiny-server/run
+# Create log, config, and application directories
+sudo mkdir -p /var/log/shiny-server
+sudo mkdir -p /srv/shiny-server
+sudo mkdir -p /var/lib/shiny-server
+sudo chown shiny /var/log/shiny-server
+sudo mkdir -p /etc/shiny-server
 
-# install init script
-cp /rocker_scripts/init_set_env.sh /etc/cont-init.d/01_set_env
-
-# Clean up
-rm -rf /var/lib/apt/lists/*
-rm -rf /tmp/downloaded_packages
-
-## Strip binary installed lybraries from RSPM
-## https://github.com/rocker-org/rocker-versioned2/issues/340
-strip /usr/local/lib/R/site-library/*/libs/*.so
